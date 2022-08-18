6324f475c81b5d7e32111f9cafcac7d018e9da00
saniya1202 <105457339+saniya1202@users.noreply.github.com>
2022-08-18 00:12:04 -0400

    Lab 5

---
+++ powershell/powershell-lab5.ps1
@@ -0,0 +1,24 @@
+﻿param ( [switch]$System, [switch]$Disks, [switch]$Network)
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+  Hardware  
+  OperatingSystem
+  Processor
+  RAMSummary
+  VideoController
+  PhysicalDrives
+  NetworkAdapter
+}
+if ($System) {   
+  Hardware  
+  OperatingSystem
+  Processor
+  RAMSummary
+  VideoController
+}
+if ($Disks) { 
+  PhysicalDrives
+}
+if ($Network) { 
+  NetworkAdapter
+}
